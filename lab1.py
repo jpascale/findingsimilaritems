ff36fa7cfa6ac4ba4eb65d3c2b1fd6d44481995d
jpascale <jpascale@itba.edu.ar>
2016-11-17 05:37:07 +0100

    Generate shinglings without hash

---
+++ lab1.py
@@ -0,0 +1,41 @@
+#Tested
+def line_hashed_shingle(tokens, k):
+	arr = []
+	if len(tokens)%2 == 1:
+		max_i = len(tokens) - k
+	else:
+		max_i = len(tokens) - k + 1
+
+	for i in range(max_i):
+		arr.append(tokens[i:i+k])
+
+	return arr
+
+#Tested
+def make_hashed_shingles(textFile, k):
+	#import pdb; pdb.set_trace()
+	arr = []
+	size = textFile.count()
+	bff = []
+
+	for i in range(size):
+		line = textFile.filter(lambda (x, y) : y == i).map(lambda (x,y) : x).collect()[0]
+		line = bff + line
+		arr = arr + line_hashed_shingle(line, k)
+		bff = line[-(k-1):]
+
+	return arr
+
+class Shingling(object):
+
+	def __init__(self, filename, k):
+		self.k = k
+		self.filename = filename
+
+		textFile = sc.textFile(filename)
+		textFile = textFile.map(lambda x: x.split()).zipWithIndex()
+		self.arr = make_hashed_shingles(textFile, k)
+
+
+	def __str__(self):
+		return self.filename + ": " + self.k + "-Shingle"
